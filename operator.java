77bef510086144d7092939e1fdd801e597da62de
RITUURAJ <126504721+RITUURAJ@users.noreply.github.com>
2023-10-03 22:07:39 +0530

    Add files via upload
    2-day

---
+++ operator.java
@@ -0,0 +1,15 @@
+public class operator {
+    public static void main(String[] args) {
+        int p = 10;
+        int r = 10;
+        int t = 10;
+        int si = (p * r * t) / 100;
+        System.out.println(si);
+        int R = 7;
+        int area = (22 / 7) * R * R;
+        System.out.println(area);
+
+    }
+}
+
+// Date -03/10/2023
