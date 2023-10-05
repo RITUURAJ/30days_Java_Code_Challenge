f917b3666f38b8139b313032829b965e2477e7a1
RITUURAJ <126504721+RITUURAJ@users.noreply.github.com>
2023-10-05 22:50:21 +0530

    Add files via upload

--- string.java
+++ string.java
@@ -1,51 +1,51 @@
-// import java.util.*;
+import java.util.*;
 
-//  public class string {
-//      public static void main(String[] args) {
-//          Scanner sc = new Scanner(System.in);
-//          String name = sc.nextLine(); // If you use sc.next then compiler will give you single word so we nextline then compiler will give you string.
-//         System.out.println("Your name is :" + name);
-//     }
-//  }
+public class string {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         String name = sc.nextLine(); // If you use sc.next then compiler will give you single word so we nextline then compiler will give you string.
+        System.out.println("Your name is :" + name);
+    }
+ }
 
 // write a code add two name first name and last name.
 // import java.util.*;
 
-// public class string {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         String firstname = "Ritu";
-//         String lastname = "Raj";
-//         String fullname = firstname + " " + lastname;
-//         System.out.println(firstname + " " + lastname);
-//         // print length of full name
-//         System.out.println(fullname.length());
+public class string {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstname = "Ritu";
+        String lastname = "Raj";
+        String fullname = firstname + " " + lastname;
+        System.out.println(firstname + " " + lastname);
+        // print length of full name
+        System.out.println(fullname.length());
 
-//         // print character of fullname
+        // print character of fullname
 
-//         for (int i = 0; i < fullname.length(); i++) {
-//             System.out.println(fullname.charAt(i));
-//         }
-//     }
-// }
+        for (int i = 0; i < fullname.length(); i++) {
+            System.out.println(fullname.charAt(i));
+        }
+    }
+}
 
 // Write a code Compare two string
 
 // import java.util.*;
 
-// public class string {
-//     public static void main(String[] args) {
-//         String s1 = "Piyush";
-//         String s2 = "RituRaj";
-//         if (s1.compareTo(s2) == 0) {
-//             System.out.println("Strings are equal");
-//         } else {
-//             System.out.println("Strings are not equal");
-//         }
-//     }
-// }
+public class string {
+    public static void main(String[] args) {
+        String s1 = "Piyush";
+        String s2 = "RituRaj";
+        if (s1.compareTo(s2) == 0) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
 
-//write a code for find substring
+// write a code for find substring
 
 import java.util.*;
 
