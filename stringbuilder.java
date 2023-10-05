1b962ffdef556112477a9c20aa27ad16b3615fa8
RITUURAJ <126504721+RITUURAJ@users.noreply.github.com>
2023-10-05 22:54:05 +0530

    stringbuilder.java

--- stringbuilder.java
+++ stringbuilder.java
@@ -1,61 +1,61 @@
-// import java.util.*;
+import java.util.*;
 
-// public class stringbuilder {
-//     public static void main(String[] args) {
-//         StringBuilder name = new StringBuilder("Ritu");
-// System.out.println(name);
-// char at index 0
-//         System.out.println(name.charAt(0));
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder name = new StringBuilder("Ritu");
+System.out.println(name);
+char at index 0
+        System.out.println(name.charAt(0));
 
-//         // set char at index 0
-//         name.setCharAt(0, 'K');
-//         System.out.println(name);
-//     }
+        // set char at index 0
+        name.setCharAt(0, 'K');
+        System.out.println(name);
+    }
 
-// }
+}
 
 // use insert function to insert your character i at 2 place
 
-// import java.util.*;
+import java.util.*;
 
-// public class stringbuilder {
-//     public static void main(String[] args) {
-//         StringBuilder name = new StringBuilder("Ritu");
-//         System.out.println(name);
-//         name.insert(2, 'i');
-//         System.out.println(name);
-//     }
-// }
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder name = new StringBuilder("Ritu");
+        System.out.println(name);
+        name.insert(2, 'i');
+        System.out.println(name);
+    }
+}
 
 // use delete function to delete your character
 
-// import java.util.*;
+import java.util.*;
 
-// public class stringbuilder {
-//     public static void main(String[] args) {
-//         StringBuilder name = new StringBuilder("Ritu");
-//         name.insert(2, 'i');
-//         System.out.println(name);
-//         name.delete(2, 3);
-//         System.out.println(name);
-//     }
-// }
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder name = new StringBuilder("Ritu");
+        name.insert(2, 'i');
+        System.out.println(name);
+        name.delete(2, 3);
+        System.out.println(name);
+    }
+}
 
 // use new string and add character at the end of string by using append function
 //(append) means add some new after the string
 
-// import java.util.*;
+import java.util.*;
 
-// public class stringbuilder {
-//     public static void main(String[] args) {
-//         StringBuilder name = new StringBuilder("H");
-//         name.append("e");
-//         name.append("l");
-//         name.append("l");
-//         name.append("o");
-//         System.out.println(name);
-//     }
-// }
+public class stringbuilder {
+    public static void main(String[] args) {
+        StringBuilder name = new StringBuilder("H");
+        name.append("e");
+        name.append("l");
+        name.append("l");
+        name.append("o");
+        System.out.println(name);
+    }
+}
 
 // write a program to reverse your string
 
@@ -75,3 +75,5 @@ public class stringbuilder {
         System.out.println(name);
     }
 }
+
+Date - 05/10/2023
